31fb23c8d4f3967a292bcc3c489577078f702067
chaitnapatil155-spec <chaitnapatil155@gmail.com>
2026-08-26 13:57:15 +0530

    Add brand builder script for user input and bio generation

---
+++ week 1/brand_builder.py
@@ -0,0 +1,7 @@
+# brand build tool
+print(" ---- Welcome to brand builder ----")
+name = input("enter your good name:- ")
+skill = input("what is your top skiils? ")
+dream = input("what are you learning? ")
+bio = f"Hi, i am {name}. i am expert in {skill}. currently, i am learning {dream} to build the future"
+print(bio) 
